ee6275b5282cfbc7453deaa1b3ee012d8a4ab7c1
Guillaume Vadnais <guillaume.vadnais.cemt@ssss.gouv.qc.ca>
2025-06-20 09:36:18 -0400

    06_intro_to_programming lab completed

--- python-101-main/06_intro-to-programming/06_05_program_pseudocode.py
+++ python-101-main/06_intro-to-programming/06_05_program_pseudocode.py
@@ -6,3 +6,15 @@
 # You can also add real Python code after learning the necessary concepts.
 # For now, just practice breaking larger tasks into smaller steps
 # and writing out your thoughts in pseudocode.
+
+
+
+##Concept - Create an app that automates the creation of weekly grocery lists in relation to grocery's discounts and predetermined recipes
+#create a recipe database containing the ingredients, the preparation time, the amount of portions, an healthy score and a score on the delicious scale
+#Make an interface in witch I can put more details about my weekly needs (portions, time budget to cook this week, etc)
+#create some kind of automation that grabs the mains discounts from specified groceries
+#Make the app choose the optimal weekly menu based on discounts, time necessary to cook, good average on the delicious scale
+#Make the grocery list for me
+#send me the list by email
+
+
