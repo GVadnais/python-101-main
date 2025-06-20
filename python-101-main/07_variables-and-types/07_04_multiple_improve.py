81ee1152e15f784942c15a563649edcba46acdf5
Guillaume Vadnais <guillaume.vadnais.cemt@ssss.gouv.qc.ca>
2025-06-20 16:00:42 -0400

    07_variables-and-types Labs completed

--- python-101-main/07_variables-and-types/07_04_multiple_improve.py
+++ python-101-main/07_variables-and-types/07_04_multiple_improve.py
@@ -2,3 +2,9 @@
 # Why should you change it?
 
 color, year = "red", 1900
+
+color = "red"
+year = 1900
+
+
+# Why should you change it? : Assigning several variables on one line is not recommended unless there's a clear link between the 2 variables conceptially speaking (min - max  or  start - end)
--- python-101-main/07_variables-and-types/07_06_whats_my_type.py
+++ python-101-main/07_variables-and-types/07_06_whats_my_type.py
@@ -1,7 +1,18 @@
-# What data type is the variable `mystery` at the end of the script?
-# What data types does it hold at certain points earlier in the script?
+# What data type is the variable `mystery` at the end of the script?            float
+# What data types does it hold at certain points earlier in the script?         NoneType, string, integer
 
 mystery = None
+x = 1
+
+print("type", x, "is", type(mystery))
+x = x + 1
 mystery = "Sommerfeld"
+print("type", x, "is", type(mystery))
+x = x + 1
 mystery = 137
+print("type", x, "is", type(mystery))
+x = x + 1
 mystery = mystery + 0.0
+print("type", x, "is", type(mystery))
+
+##questions : why do I get a space after type in my results ?
